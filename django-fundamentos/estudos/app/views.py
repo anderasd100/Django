ed6aeb590b42bcae9d0cd8056d74de1f02c4629b
Anderson Abreu Rabelo <andersonabreurabelo.9@gmail.com>
2025-01-30 15:14:08 -0400

    Estudo DJANGO VIEWS

--- django-fundamentos/estudos/app/views.py
+++ django-fundamentos/estudos/app/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+def horario_atual(request): 
+    horario_atual = datetime.datetime.now()
+    return HttpResponse(horario_atual) 
