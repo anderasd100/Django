7fb7ea49ae2f378201d9f393a8b1f8bc15aaeb0b
Anderson Abreu Rabelo <andersonabreurabelo.9@gmail.com>
2025-03-19 12:04:28 -0400

    Atualizações 19.03.2025

---
+++ Projeto 1/contas/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+# Create your views here.
+
+def home(request):
+    now = datetime.datetime.now()
+   # html = "<html><body>It is now %s.</body></html>" % now
+    return render(request, 'contas/home.html')
+
+
+#Uma Views é função ou uma classe
